9269577a8f43e41ae6bb26ae6d24de8bd3461b96
mohit mehra <786hitmen@gmail.com>
2025-11-21 17:51:13 +0530

    3rd coomit

--- server.ts
+++ server.ts
@@ -2,7 +2,7 @@ import "dotenv/config";
 import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
 import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
 import z from "zod";
-// import express from "express";
+import express from "express";
 import { MongoClient, ObjectId } from "mongodb";
 import {
   classifyIdentifier,
